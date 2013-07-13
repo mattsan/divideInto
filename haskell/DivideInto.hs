003cb9aae94df804d4adb7dddafbe2fdf1a0a80e
Eiji MATSUMOTO <e.mattsan@gmail.com>
2013-07-13 21:42:35 +0900

    unfoldr version

--- haskell/DivideInto.hs
+++ haskell/DivideInto.hs
@@ -1,10 +1,12 @@
 module DivideInto where
 
+import Data.List
+
 divideInto :: Int -> [a] -> [[a]]
 divideInto n xs = split (((length xs) + n - 1) `div` n) xs
 
 split :: Int -> [a] -> [[a]]
-split _ [] = []
-split m xs = xs1:(split m xs2)
+split m = unfoldr phi
   where
-    (xs1, xs2) = splitAt m xs
+    phi [] = Nothing
+    phi xs = Just $ splitAt m xs
