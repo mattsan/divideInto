07d9744aa2582e97134c6744ce7113c8de0e8bc9
Eiji MATSUMOTO <e.mattsan@gmail.com>
2013-07-13 23:49:52 +0900

    remove split

--- haskell/DivideInto.hs
+++ haskell/DivideInto.hs
@@ -3,10 +3,5 @@ module DivideInto where
 import Data.List
 
 divideInto :: Int -> [a] -> [[a]]
-divideInto n xs = split (((length xs) + n - 1) `div` n) xs
-
-split :: Int -> [a] -> [[a]]
-split m = unfoldr phi
-  where
-    phi [] = Nothing
-    phi xs = Just $ splitAt m xs
+divideInto n xs = unfoldr (\xs -> if null xs then Nothing else Just $ splitAt m xs) xs
+  where m = ((length xs) + n - 1) `div` n
